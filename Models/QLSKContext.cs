0c3519cc04eb26c1b12238b2019a66d6b5a0682c
Nguyễn Trung Kiên <179913049+kienkohai1@users.noreply.github.com>
2025-06-28 23:14:40 +0700

    Add files via upload

---
+++ Models/QLSKContext.cs
@@ -0,0 +1,14 @@
+﻿using BTL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BTL.Models
+{
+    public class QLSKContext : DbContext
+    {
+        public QLSKContext(DbContextOptions<QLSKContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; } // Đại diện cho bảng Events trong cơ sở dữ liệu của bạn
+    }
+}
